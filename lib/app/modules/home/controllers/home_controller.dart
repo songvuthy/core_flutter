1ea7ae15caec182dc5a6b4a2c14791e450efe732
Song Vuthy <songvuthy93@gmail.com>
2025-02-19 10:26:38 +0700

    testing design ui

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,6 +1,43 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 class HomeController extends GetxController {
+  // Scroll controllers
+  final ItemScrollController itemScrollController = ItemScrollController();
+  final ItemPositionsListener itemPositionsListener =
+      ItemPositionsListener.create();
+  // Active menu index
+  var selectedMenuIndex = 0.obs;
+  // Menu and section data
+  final List<String> menuItems = ['Hits', 'Rolls', 'Boxes', 'Drinks', 'Sets'];
+  final List<String> sections = [
+    'Hits Section',
+    'Rolls Section',
+    'Boxes Section',
+    'Drinks Section',
+    'Sets Section'
+  ];
 
-  
+  @override
+  void onInit() {
+    super.onInit();
+    // Update active menu when scrolling
+    itemPositionsListener.itemPositions.addListener(() {
+      final visibleItems = itemPositionsListener.itemPositions.value;
+      if (visibleItems.isNotEmpty) {
+        int firstVisibleIndex = visibleItems.first.index;
+        selectedMenuIndex.value = firstVisibleIndex;
+      }
+    });
+  }
+
+  // Scroll to specific section
+  void scrollToSection(int index) {
+    itemScrollController.scrollTo(
+      index: index,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,10 +1,10 @@
-import 'package:core_flutter/app/modules/home/views/widgets/home_widget.dart';
 import 'package:core_flutter/app/routes/app_pages.dart';
 import 'package:core_flutter/app/translations/app_language_key_extension.dart';
 import 'package:core_flutter/app/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -19,8 +19,91 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-          isShowBack: false, title: Text(LanguageKey.homeTitle.tr)),
-      body: HomeWidget(controller: controller),
+        isShowBack: false,
+        title: Text(LanguageKey.homeTitle.tr),
+      ),
+      body: Row(
+        children: [
+          // Pinned Menu without wrapping the entire ListView
+          Container(
+            width: 100,
+            color: Colors.orange[100],
+            child: ListView.builder(
+              itemCount: controller.menuItems.length,
+              itemBuilder: (context, index) {
+                return Obx(() => ListTile(
+                      title: Column(
+                        children: [
+                          Icon(
+                            Icons.circle,
+                            color: controller.selectedMenuIndex.value == index
+                                ? Colors.orange
+                                : Colors.grey,
+                          ),
+                          Text(
+                            controller.menuItems[index],
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: controller.selectedMenuIndex.value == index
+                                  ? Colors.orange
+                                  : Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                      onTap: () {
+                        // Scroll to the section on tap
+                        controller.scrollToSection(index);
+                      },
+                    ));
+              },
+            ),
+          ),
+          // Scrollable List of Sections
+          Expanded(
+            child: ScrollablePositionedList.builder(
+              itemScrollController: controller.itemScrollController,
+              itemPositionsListener: controller.itemPositionsListener,
+              itemCount: controller.sections.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  height: 300,
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(bottom: 10),
+                  color: Colors.orange[50 * (index + 1)],
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        controller.sections[index],
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text('Description for ${controller.sections[index]}'),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 60,
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('My Order'),
+              Text('Total: 0'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
